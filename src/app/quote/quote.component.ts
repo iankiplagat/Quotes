868983c05fc744641bd921f5ca70c1b5e08e7ed5
Kasparov <ianjkiplagat@gmail.com>
2021-03-26 14:21:34 +0300

    add delete function

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -37,9 +37,7 @@ export class QuoteComponent implements OnInit {
 
   deleteQuote(isComplete, index) {
     if (isComplete) {
-      let toDelete = confirm(
-        `Are you sure you want to delete ${this.quotes[index].name}?`
-      );
+      let toDelete = confirm(`Are you sure you want to delete this quote?`);
 
       if (toDelete) {
         this.quotes.splice(index, 1);
