a6edc8f887acc3228641ae57840512ed69ac5f04
Kasparov <ianjkiplagat@gmail.com>
2021-03-26 06:23:07 +0300

    add post date status

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -10,23 +10,28 @@ export class QuoteComponent implements OnInit {
   quotes: Quote[] = [
     new Quote(
       1,
-      '"The greatest glory in living lies not in never falling, but in rising every time we fall." -Nelson Mandela'
+      '"The greatest glory in living lies not in never falling, but in rising every time we fall." -Nelson Mandela',
+      new Date(2018, 4, 27)
     ),
     new Quote(
       2,
-      "\"If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough.\" -Oprah Winfrey"
+      "\"If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough.\" -Oprah Winfrey",
+      new Date(2018, 4, 27)
     ),
     new Quote(
       3,
-      '"It is during our darkest moments that we must focus to see the light." -Aristotle'
+      '"It is during our darkest moments that we must focus to see the light." -Aristotle',
+      new Date(2018, 4, 27)
     ),
     new Quote(
       4,
-      '"The future belongs to those who believe in the beauty of their dreams." -Eleanor Roosevelt'
+      '"The future belongs to those who believe in the beauty of their dreams." -Eleanor Roosevelt',
+      new Date(2018, 4, 27)
     ),
     new Quote(
       5,
-      '"Life is really simple, but we insist on making it complicated." -Confucius'
+      '"Life is really simple, but we insist on making it complicated." -Confucius',
+      new Date(2018, 4, 27)
     ),
   ];
 
