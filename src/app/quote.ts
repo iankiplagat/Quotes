79b39ccdb4ac326648bd446527e666eb673e649c
Kasparov <ianjkiplagat@gmail.com>
2021-03-26 05:52:44 +0300

    add quote constructor

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,4 +1,3 @@
 export class Quote {
-  id: number;
-  name: string;
+  constructor(public id: number, public name: string) {}
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,11 +8,26 @@ import { Quote } from '../quote';
 })
 export class QuoteComponent implements OnInit {
   quotes: Quote[] = [
-    { id: 1, name: '\"The greatest glory in living lies not in never falling, but in rising every time we fall.\" -Nelson Mandela' },
-    { id: 2, name: '\"If you look at what you have in life, you\'ll always have more. If you look at what you don\'t have in life, you\'ll never have enough.\" -Oprah Winfrey' },
-    { id: 3, name: '\"It is during our darkest moments that we must focus to see the light.\" -Aristotle' },
-    { id: 4, name: '\"The future belongs to those who believe in the beauty of their dreams.\" -Eleanor Roosevelt' },
-    { id: 5, name: '\"Life is really simple, but we insist on making it complicated.\" -Confucius' },
+    new Quote(
+      1,
+      '"The greatest glory in living lies not in never falling, but in rising every time we fall." -Nelson Mandela'
+    ),
+    new Quote(
+      2,
+      "\"If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough.\" -Oprah Winfrey"
+    ),
+    new Quote(
+      3,
+      '"It is during our darkest moments that we must focus to see the light." -Aristotle'
+    ),
+    new Quote(
+      4,
+      '"The future belongs to those who believe in the beauty of their dreams." -Eleanor Roosevelt'
+    ),
+    new Quote(
+      5,
+      '"Life is really simple, but we insist on making it complicated." -Confucius'
+    ),
   ];
 
   constructor() {}
