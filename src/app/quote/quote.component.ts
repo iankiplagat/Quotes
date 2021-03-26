35013ce159fa6b37e7c1b395489ab9442662b7ec
Kasparov <ianjkiplagat@gmail.com>
2021-03-26 08:42:06 +0300

    add quotes entry form

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -35,6 +35,18 @@ export class QuoteComponent implements OnInit {
     ),
   ];
 
+  deleteQuote(isComplete, index) {
+    if (isComplete) {
+      let toDelete = confirm(
+        `Are you sure you want to delete ${this.quotes[index].name}?`
+      );
+
+      if (toDelete) {
+        this.quotes.splice(index, 1);
+      }
+    }
+  }
+
   constructor() {}
 
   ngOnInit() {}
